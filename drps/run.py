204bc34a3a872da4a76b2add9fa6aafbdd4afeb0
Ghazi Zabalawi <46258765+ghazigamer@users.noreply.github.com>
2021-12-29 12:58:49 +1300

    changed from django-rest-framework to djangorestframework when installing the dependency

--- drps/run.py
+++ drps/run.py
@@ -20,7 +20,7 @@ def start_project():
     react_app_name = react_project_app_name.split(",")[1]
 
     venv_name = input("Please Enter the name of your virtual environment : ")
-    python_command = f"cd {path} &&  mkdir {project_name} && cd {path}\\{project_name} && django-admin startproject {django_project_name} && cd {path}\\{project_name}\\{django_project_name} && python -m venv {venv_name} && django-admin startapp {django_app_name} &&  {venv_name}\\Scripts\\activate && python -m pip install django && python -m pip install django-rest-framework && python -m pip install django-cors-headers && python -m pip install django-extensions"
+    python_command = f"cd {path} &&  mkdir {project_name} && cd {path}\\{project_name} && django-admin startproject {django_project_name} && cd {path}\\{project_name}\\{django_project_name} && python -m venv {venv_name} && django-admin startapp {django_app_name} &&  {venv_name}\\Scripts\\activate && python -m pip install django && python -m pip install djangorestframework && python -m pip install django-cors-headers && python -m pip install django-extensions"
     js_command = f"cd {path}\\{project_name} && mkdir {react_project_name} && cd {path}\\{project_name}\\{react_project_name} && npx create-react-app {react_app_name} && npm install axios && npm install react-router-dom"
     os.system(python_command)
     os.system(js_command)
