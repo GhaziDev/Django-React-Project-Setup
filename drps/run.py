d7a6810d84c8e4e522f258559ad11a4df89edc86
Ghazi Zabalawi <46258765+ghazigamer@users.noreply.github.com>
2021-12-29 18:56:38 +1300

    formatted code to follow PEP-8 rules

--- drps/run.py
+++ drps/run.py
@@ -1,10 +1,10 @@
 import os
 
 def start_project():
-    path: str = input("Please Enter the path of your project : ")
+    path = input("Please Enter the path of your project : ")
     is_path = os.path.exists(path)
-    while is_path == False:
-        path: str = input("Invalid Path. Please Enter the path of your project : ")
+    while is_path is False:
+        path = input("Invalid Path. Please Enter the path of your project : ")
         is_path = os.path.exists(path)
     project_name = input("What the name of the project : ")
     django_project_app = input(
@@ -20,10 +20,27 @@ def start_project():
     react_app_name = react_project_app_name.split(",")[1]
 
     venv_name = input("Please Enter the name of your virtual environment : ")
-    python_command = f"cd {path} &&  mkdir {project_name} && cd {path}\\{project_name} && django-admin startproject {django_project_name} && cd {path}\\{project_name}\\{django_project_name} && python -m venv {venv_name} && django-admin startapp {django_app_name} &&  {venv_name}\\Scripts\\activate && python -m pip install django && python -m pip install djangorestframework && python -m pip install django-cors-headers && python -m pip install django-extensions"
-    js_command = f"cd {path}\\{project_name} && mkdir {react_project_name} && cd {path}\\{project_name}\\{react_project_name} && npx create-react-app {react_app_name} && npm install axios && npm install react-router-dom"
+    python_command = (
+        f"cd {path} &&  mkdir {project_name} "
+        f"&& cd {path}\\{project_name} "
+        f"&& django-admin startproject {django_project_name} "
+        f"&& cd {path}\\{project_name}\\{django_project_name} "
+        f"&& python -m venv {venv_name} "
+        f"&& django-admin startapp {django_app_name} "
+        f"&&  {venv_name}\\Scripts\\activate "
+        f"&& python -m pip install django"
+        f"&& python -m pip install djangorestframework "
+        f"&& python -m pip install django-cors-headers "
+        f"&& python -m pip install django-extensions"
+        )
+    js_command = (
+        f"cd {path}\\{project_name} " 
+        f"&& mkdir {react_project_name} "
+        f"&& cd {path}\\{project_name}\\{react_project_name} "
+        f"&& npx create-react-app {react_app_name} "
+        f"&& npm install axios && npm install react-router-dom"
+    )
     os.system(python_command)
     os.system(js_command)
     print("Project Created Successfully")
-
 
